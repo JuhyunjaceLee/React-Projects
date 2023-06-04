cfa7eb3fd3d0aa98057fa4a87faa24ed9868e0ff
JuhyunjaceLee <heyjace777@gmail.com>
2023-06-04 23:57:32 +0900

    homepage constructuring

--- portfolio-page/src/pages/Homepage.jsx
+++ portfolio-page/src/pages/Homepage.jsx
@@ -10,21 +10,30 @@ export default function Homepage() {
       <div className={styles.home_contents}>
         <h1 className={styles.home_title}>FRONT-END DEVELOPER</h1>
         <main className={`${styles.home_main} ${styles.mainbox1}`}>
-          <div>
+          <div className={styles.home_eachBox}>
             <div className={styles.home_aboutBox_contents}>
               <p>안녕하세요!</p>
               <p>
                 <span className={styles.home_myName}>이주현</span>입니다.😊
               </p>
-              <p>"Front-End Developer"</p>
+              <p className={styles.home_frontend}>"Front-End Developer"</p>
             </div>
             <BsFillArrowRightCircleFill />
           </div>
-          <div>project</div>
+          <div className={styles.home_eachBox}>
+            <p>PROJECTS</p>
+            <BsFillArrowRightCircleFill />
+          </div>
         </main>
         <main className={`${styles.home_main} ${styles.mainbox2}`}>
-          <div>skill</div>
-          <div>contact</div>
+          <div className={styles.home_eachBox}>
+            <p>SKILLS</p>
+            <BsFillArrowRightCircleFill />
+          </div>
+          <div className={styles.home_eachBox}>
+            <p>CONTACT</p>
+            <BsFillArrowRightCircleFill />
+          </div>
         </main>
       </div>
     </div>
