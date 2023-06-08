d983f1b601277c29664afbaacd4993bbec72aaa2
JuhyunjaceLee <heyjace777@gmail.com>
2023-06-08 22:18:28 +0900

    add projectPage

--- portfolio-page/src/pages/Aboutpage.jsx
+++ portfolio-page/src/pages/Aboutpage.jsx
@@ -35,34 +35,83 @@ export default function Aboutpage() {
             </div>
           </div>
         </div>
-        <div className={styles.education_contents}>
-          <h2 className={styles.education_title}>EDUCATION</h2>
-          <div className={styles.education_textbox_up}>
-            <div className={styles.education_textbox}>
-              <h4>프론트엔드웹개발 양성 과정 수료</h4>
-              <p>-HTML5, CSS, 자바스크립트, 제이쿼리, 리액트</p>
-              <p>-넵플릭스 국비교육기관</p>
-              <div className={styles.education_circle_up}></div>
-              <div className={styles.education_arrow_down}></div>
+        <div className={styles.about_contents}>
+          <h2 className={styles.about_contents_title}>EDUCATION</h2>
+          <div className={styles.timeline}></div>
+          <div className={styles.timeline_arrow}></div>
+          <div className={styles.textbox_contents}>
+            <div className={styles.textbox_wrap}>
+              <div className={`${styles.textbox} ${styles.textbox_up}`}>
+                <h3 className={styles.textbox_title}>
+                  프론트엔드 웹 개발 양성 과정 수료
+                </h3>
+                <p>-HTML5, CSS, 자바스크립트, 제이쿼리, 리액트</p>
+                <p>-넵플릭스 국비교육기관</p>
+                <div className={styles.circle_up}></div>
+                <div className={styles.arrow_down}></div>
+              </div>
+              <p className={styles.date_up}>2022.02 - 2022.03</p>
             </div>
-            <div className={styles.education_textbox}>
-              <h4>자바스크립트 & Vue.js</h4>
-              <p>-자바스크립트 및 Vue.js 프레임워크 이론 및 실습 교육</p>
-              <p>-마포청년나루</p>
-              <div className={styles.education_circle_up}></div>
-              <div className={styles.education_arrow_down}></div>
+            <div className={styles.textbox_wrap}>
+              <p className={styles.down_date}>2022.04 - 2022.05</p>
+              <div className={`${styles.textbox} ${styles.textbox_down}`}>
+                <h3 className={styles.textbox_title}>
+                  자바스크립트 프로젝트 & 포트폴리오 제작과정
+                </h3>
+                <p>
+                  -클론코딩을 통하여 포트폴리오로 활용할 수 있는 프로젝트 과정
+                </p>
+                <p>-마포청년나루</p>
+                <div className={styles.circle_down}></div>
+                <div className={styles.arrow_up}></div>
+              </div>
             </div>
-            <div className={styles.education_timeline}></div>
+            <div className={styles.textbox_wrap}>
+              <div className={`${styles.textbox} ${styles.textbox_up}`}>
+                <h3 className={styles.textbox_title}>자바스크립트 & Vue.js</h3>
+                <p>-자바스크립트 및 Vue.js 프레임워크 이론 및 실습 교육</p>
+                <p>-마포청년나루</p>
+                <div className={styles.circle_up}></div>
+                <div className={styles.arrow_down}></div>
+              </div>
+              <p className={styles.date_up}>2022.05 - 2022.06</p>
+            </div>
           </div>
-          <div className={styles.education_textbox_down}>
-            <div className={styles.education_textbox}>
-              <h4>자바스크립트 프로젝트 & 포트폴리오 제작과정</h4>
-              <p>
-                -클론코딩을 통하여 포트폴리오로 활용할 수 있는 프로젝트 과정
-              </p>
-              <p>-마포청년나루</p>
-              <div className={styles.education_circle_down}></div>
-              <div className={styles.education_arrow_up}></div>
+        </div>
+        <div className={styles.about_contents}>
+          <h2 className={styles.about_contents_title}>EXPERIENCE</h2>
+          <div className={styles.timeline}></div>
+          <div className={styles.timeline_arrow}></div>
+          <div className={styles.textbox_contents}>
+            <div className={styles.textbox_wrap}>
+              <div className={`${styles.textbox} ${styles.textbox_up}`}>
+                <h3 className={styles.textbox_title}>주식회사 신영솔루션</h3>
+                <p>-서브PM(스마트공장 산출물 작성)</p>
+                <p>-회계</p>
+                <div className={styles.circle_up}></div>
+                <div className={styles.arrow_down}></div>
+              </div>
+              <p className={styles.date_up}>2020.10 - 2021.12</p>
+            </div>
+            <div className={styles.textbox_wrap}>
+              <p className={styles.down_date}>2017.10 - 2018.05</p>
+              <div className={`${styles.textbox} ${styles.textbox_down}`}>
+                <h3 className={styles.textbox_title}>유니클로</h3>
+                <p>-고객 응대 및 서비스</p>
+                <p>-매장 관리</p>
+                <div className={styles.circle_down}></div>
+                <div className={styles.arrow_up}></div>
+              </div>
+            </div>
+            <div className={styles.textbox_wrap}>
+              <div className={`${styles.textbox} ${styles.textbox_up}`}>
+                <h3 className={styles.textbox_title}>법무법인 원진</h3>
+                <p>-문서송부 및 관리</p>
+                <p>-회계 및 총무 담당</p>
+                <div className={styles.circle_up}></div>
+                <div className={styles.arrow_down}></div>
+              </div>
+              <p className={styles.date_up}>2022.02 - 2022.03</p>
             </div>
           </div>
         </div>
--- portfolio-page/src/pages/Projectpage.jsx
+++ portfolio-page/src/pages/Projectpage.jsx
@@ -1,9 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import commonstyles from "./Common.module.css";
+import styles from "./Projectpage.module.css";
 
 export default function Projectpage() {
+  const buttons = ["JS", "VUE", "REACT"];
+  const lists = [
+    {
+      imgSrc: "image/thumbnail/closet_thumbnail.png",
+      name: "What`s In My Closet",
+      type: "JS",
+    },
+    {
+      imgSrc: "image/thumbnail/shoppingList_thumbnail.png",
+      name: "Shopping List",
+      type: "JS",
+    },
+    {
+      imgSrc: "image/thumbnail/ploggingGame_thumbnail.png",
+      name: "Plogging Game",
+      type: "JS",
+    },
+    {
+      imgSrc: "image/thumbnail/myBox_thumbnail.png",
+      name: "My Box",
+      type: "VUE",
+    },
+    {
+      imgSrc: "image/thumbnail/weatherApp_thumbnail.png",
+      name: "Weather App",
+      type: "REACT",
+    },
+    {
+      imgSrc: "image/thumbnail/shoppingApp_thumbnail.png",
+      name: "Shopping App",
+      type: "REACT",
+    },
+  ];
+  const [allList, setAllList] = useState([]);
+  const [filterList, setFilterList] = useState([]);
+
+  const getLists = () => {
+    setAllList(lists);
+    setFilterList(lists);
+  };
+
+  const allListsHandler = () => {
+    getLists();
+  };
+
+  const filterListsHandler = (e) => {
+    const filterBtn = e.target.innerHTML;
+    const filter = allList.filter((list) => list.type === filterBtn);
+    setFilterList(filter);
+  };
+
+  useEffect(() => {
+    getLists();
+  }, []);
+
   return (
-    <div>
-      <p>Project Page</p>
+    <div className={commonstyles.contents}>
+      <h1 className={commonstyles.contents_title}>PROJECTS</h1>
+      <h2 className={styles.projects_title}>My projects</h2>
+      <ul className={styles.projects_btn_wrap}>
+        <li className={styles.projects_btn} onClick={allListsHandler}>
+          ALL
+        </li>
+        {buttons.map((btn, idx) => (
+          <li
+            className={styles.projects_btn}
+            key={idx}
+            onClick={filterListsHandler}
+          >
+            {btn}
+          </li>
+        ))}
+      </ul>
+      <main className={styles.projects_contents}>
+        {filterList.map((list, idx) => (
+          <div className={styles.projects_list} key={idx}>
+            <img
+              className={styles.projects_list_img}
+              src={list.imgSrc}
+              alt={list.name}
+            />
+            <h3>{list.name}</h3>
+            <p> 바로가기 CLICK!</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
